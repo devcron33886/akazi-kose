5c1a95f397683dca987387c413ffb09b5dea585a
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-02-14 11:33:50 +0200

    wip

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -24,8 +24,7 @@
                 <div class="w-1/2">
                     <livewire:search />
                 </div>
-
-                <div class="mt-12">
+                <div class="py-6 w-full max-w-screen-xl mx-auto">
                     <div class="overflow-hidden rounded-md border border-blue-500 bg-blue-500 p-1">
                         <ul class="flex items-center gap-2 text-sm font-medium">
                             @foreach ($categories as $category)
@@ -41,33 +40,29 @@
                             @endforeach
                         </ul>
                     </div>
-
-                    <div class="py-6 w-full max-w-screen-xl mx-auto">
-                        @foreach ($adverts as $advert)
-                            <div class="mt-2">
-                                <a href="{{ route('adverts.show', $advert->slug) }}"
-                                    class="flex flex-col items-center bg-white border border-blue-600 rounded-lg md:flex-row md:max-w-4xl">
-                                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                                        src="/storage/{{ $advert->institution->logo }}"
-                                        alt="{{ $advert->institution->name }}">
-                                    <div class="flex flex-col justify-between p-4 leading-normal">
-                                        <h5
-                                            class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                                            {{ $advert->title }}</h5>
-                                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                            {{ $advert->institution->name }}| Location: {{ $advert->location }}|
-                                            Published on: {{ $advert->formatted_date }}| Deadline:
-                                            {{ $advert->formatted_deadline }}| Experience:
-                                            {{ $advert->desired_experience ?? 'Not specified' }} </p>
-                                        <div class="max-w-2xl">
-                                            <span
-                                                class="inline-block px-2 py-1 text-xs font-semibold text-white bg-blue-500 rounded">{{ $advert->category->name }}</span>
-                                        </div>
+                    @foreach ($adverts as $advert)
+                        <div class="mt-4">
+                            <a href="{{ route('adverts.show', $advert->slug) }}"
+                                class="flex flex-col items-center bg-white border border-blue-600 rounded-lg md:flex-row md:max-w-full">
+                                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                                    src="/storage/{{ $advert->institution->logo }}"
+                                    alt="{{ $advert->institution->name }}">
+                                <div class="flex flex-col justify-between p-4 leading-normal">
+                                    <h5 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                                        {{ $advert->title }}</h5>
+                                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                        {{ $advert->institution->name }}| Location: {{ $advert->location }}|
+                                        Published on: {{ $advert->formatted_date }}| Deadline:
+                                        {{ $advert->formatted_deadline }}| Experience:
+                                        {{ $advert->desired_experience ?? 'Not specified' }} </p>
+                                    <div class="max-w-2xl">
+                                        <span
+                                            class="inline-block px-2 py-1 text-xs font-semibold text-white bg-blue-500 rounded">{{ $advert->category->name }}</span>
                                     </div>
-                                </a>
-                            </div>
-                        @endforeach
-                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
