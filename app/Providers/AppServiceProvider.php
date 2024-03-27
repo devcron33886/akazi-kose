8cc36609177734b2e7427c69efc30cd7b1c64c49
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-03-27 23:51:55 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('categories', Category::withCount('adverts')->latest()->get());
+        View::share('categories', Category::withCount('adverts')->get());
         View::share('settings', Setting::first());
         Model::unguard();
     }
