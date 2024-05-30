2237066fd643129e4207d1933918deea5de04464
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-05-31 01:43:08 +0200

    wip

--- app/Models/Traits/Multitenantable.php
+++ app/Models/Traits/Multitenantable.php
@@ -5,18 +5,20 @@ namespace App\Models\Traits;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 
-trait Multitenantable
-{
-    protected static function bootMultitenantable(): void
-    {
-        static::creating(function ($model) {
-            $model->user_id = Auth::id();
-        });
+trait Multitenantable {
+	protected static function bootMultitenantable(): void {
+		// Set the user_id when a model is being created
+		static::creating(function ($model) {
+			$model->user_id = Auth::id();
+		});
 
-        if (Auth::check() && ! Auth::user()->is_admin) {
-            static::addGlobalScope('created_by_user_id', function (Builder $builder) {
-                $builder->where('user_id', Auth::id());
-            });
-        }
-    }
+		// Add a global scope to filter results by user_id
+		if (Auth::check()) {
+			static::addGlobalScope('created_by_user_id', function (Builder $builder) {
+				if (!Auth::user()->is_admin) {
+					$builder->where('user_id', Auth::id());
+				}
+			});
+		}
+	}
 }
